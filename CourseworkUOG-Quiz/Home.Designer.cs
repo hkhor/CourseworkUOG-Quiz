3f837f5db9d1a42a1a1a63dcc06fe3824e9ad1f8
Khor Chin Hui <huikhor19990520@gmail.com>
2021-10-27 22:29:56 +0800

    remove unnessary label & button

--- CourseworkUOG-Quiz/Home.Designer.cs
+++ CourseworkUOG-Quiz/Home.Designer.cs
@@ -29,68 +29,20 @@ namespace CourseworkUOG_Quiz
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblNoOfQuestion = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblCategory = new System.Windows.Forms.Label();
-            this.lblTypeOfCategory = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // lblNoOfQuestion
-            // 
-            this.lblNoOfQuestion.AutoSize = true;
-            this.lblNoOfQuestion.Location = new System.Drawing.Point(126, 55);
-            this.lblNoOfQuestion.Name = "lblNoOfQuestion";
-            this.lblNoOfQuestion.Size = new System.Drawing.Size(35, 13);
-            this.lblNoOfQuestion.TabIndex = 0;
-            this.lblNoOfQuestion.Text = "label1";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(190, 55);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
-            // lblCategory
-            // 
-            this.lblCategory.AutoSize = true;
-            this.lblCategory.Location = new System.Drawing.Point(69, 20);
-            this.lblCategory.Name = "lblCategory";
-            this.lblCategory.Size = new System.Drawing.Size(52, 13);
-            this.lblCategory.TabIndex = 2;
-            this.lblCategory.Text = "Category:";
-            // 
-            // lblTypeOfCategory
-            // 
-            this.lblTypeOfCategory.AutoSize = true;
-            this.lblTypeOfCategory.Location = new System.Drawing.Point(127, 20);
-            this.lblTypeOfCategory.Name = "lblTypeOfCategory";
-            this.lblTypeOfCategory.Size = new System.Drawing.Size(0, 13);
-            this.lblTypeOfCategory.TabIndex = 3;
             // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.lblTypeOfCategory);
-            this.Controls.Add(this.lblCategory);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.lblNoOfQuestion);
+            this.ClientSize = new System.Drawing.Size(1045, 567);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Home";
             this.Text = "Home";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblNoOfQuestion;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblCategory;
-        private System.Windows.Forms.Label lblTypeOfCategory;
     }
 }
