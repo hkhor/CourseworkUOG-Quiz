489265f67701597a03092d7313404c10b79956ec
sweemingtan.9 <sweemingtan.9@gmail.com>
2021-11-18 00:40:01 +0800

    update the home page

--- CourseworkUOG-Quiz/Home.Designer.cs
+++ CourseworkUOG-Quiz/Home.Designer.cs
@@ -29,21 +29,122 @@ namespace CourseworkUOG_Quiz
         /// </summary>
         private void InitializeComponent()
         {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(217, 173);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(174, 39);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "JungleAnimal";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(427, 173);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 39);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Bird";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(555, 173);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(186, 39);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "AquaticAnimal";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(236, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 55);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Zoology Quiz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 49);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Category :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 63);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 63);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Home";
             this.Text = "Home";
             this.Load += new System.EventHandler(this.Home_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- CourseworkUOG-Quiz/Home.cs
+++ CourseworkUOG-Quiz/Home.cs
@@ -13,14 +13,7 @@ namespace CourseworkUOG_Quiz
 {
     public partial class Home : Form
     {
-        public Button button1;
-        public Button button2;
-        public RadioButton radioButton1;
-        public RadioButton radioButton2;
-        public RadioButton radioButton3;
-        public Label label1;
-
-        
+      
         public Home()
         {
             InitializeComponent();
@@ -30,74 +23,32 @@ namespace CourseworkUOG_Quiz
 
         private void Home_Load(object sender, EventArgs e)
         {
-            button1 = new Button();
-            button1.Size = new Size(100, 40);
-            button1.Location = new Point(125, 200);
-            button1.Text = "Start Quiz";
-            this.Controls.Add(button1);
-            button1.Click += new EventHandler(button1_click);
 
-            button2 = new Button();
-            button2.Size = new Size(100, 40);
-            button2.Location = new Point(375, 200);
-            button2.Text = "Exit";
-            this.Controls.Add(button2);
-            button2.Click += new EventHandler(button2_click);
-
-            radioButton1 = new RadioButton();
-            radioButton1.Size = new Size(100, 100);
-            radioButton1.Location = new Point(120, 100);
-            radioButton1.Text = "JungleAnimal";
-            this.Controls.Add(radioButton1);
-            radioButton1.Click += new EventHandler(radioButton1_click);
-
-            radioButton2 = new RadioButton();
-            radioButton2.Size = new Size(100, 100);
-            radioButton2.Location = new Point(250, 100);
-            radioButton2.Text = "Bird";
-            this.Controls.Add(radioButton2);
-            radioButton2.Click += new EventHandler(radioButton2_click);
-
-            radioButton3 = new RadioButton();
-            radioButton3.Size = new Size(100, 100);
-            radioButton3.Location = new Point(370, 100);
-            radioButton3.Text = "AquaticAnimal";
-            this.Controls.Add(radioButton3);
-            radioButton3.Click += new EventHandler(radioButton3_click);
-
-            label1 = new Label();
-            label1.Size = new Size(250, 250);
-            label1.Location = new Point(250, 50);
-            label1.Text = "Zoology QUIZ";
-            this.Controls.Add(label1);
-            label1.Click += new EventHandler(label1_click);
 
         }
-
-
-        
-        private void button1_click(object sender, EventArgs e)
+   
+     
+        private void button1_Click_1(object sender, EventArgs e)
         {
-
             if (radioButton1.Checked == true)
             {
-                var newform = new JungleAnimal();
+                var newform = new Animation();
                 newform.Show();
             }
 
-
             else if (radioButton2.Checked == true)
             {
-                var newform = new Bird();
+                var newform = new Animation();
                 newform.Show();
             }
             else if (radioButton3.Checked == true)
             {
-                var newform = new AquaticAnimal();
+                var newform = new Animation();
                 newform.Show();
             }
         }
-        private void button2_click(object sender, EventArgs e)
+
+        private void button2_Click(object sender, EventArgs e)
         {
             DialogResult result = MessageBox.Show("Are you sure you wish to quit?", "Exit Application", MessageBoxButtons.YesNo);
             if (result == System.Windows.Forms.DialogResult.Yes)
@@ -105,46 +56,22 @@ namespace CourseworkUOG_Quiz
                 Application.Exit();
             }
         }
+    }
 
-        private void radioButton1_click(object sender, EventArgs e)
-        {
 
-        }
 
-        private void radioButton2_click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton3_click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_click(object sender, EventArgs e)
-        {
-
-        }
-
-        [STAThread]
-        static void main()
-        {
-            Application.EnableVisualStyles();
-            Application.Run(new Home());
-        }
-
-        public partial class JungleAnimal : Form
+    public partial class Animation : Form
         {
             public Label label1;
             public Label label2;
             public Timer timer1;
 
-            public JungleAnimal()
+            public Animation()
 
             {
 
                 label1 = new Label();
-                label1.Font= new Font("ALGERIAN", 30, FontStyle.Bold);
+                label1.Font = new Font("ALGERIAN", 30, FontStyle.Bold);
                 label1.Size = new Size(150, 150);
                 label1.Location = new Point(115, 90);
                 label1.Text = "00";
@@ -172,31 +99,31 @@ namespace CourseworkUOG_Quiz
             int len = 0;
             string txt;
             int timeleft = 30;
-            private void OnTimerEvent(object sender,EventArgs e)
+            private void OnTimerEvent(object sender, EventArgs e)
             {
                 if (timeleft > 0)
                 {
                     timeleft = timeleft - 1;
                     label1.Text = timeleft + "";
                 }
-                
+
                 if (label1.Text == "0")
                 {
-                    var Form1 = new Form1();
+                    var Question = new Question();
                     Hide();
-                    Form1.Show();
+                    Question.Show();
                     Close();
 
                 }
 
                 counter++;
-                if(counter > len)
+                if (counter > len)
                 {
                     counter = 0;
                     label2.Text = "";
                 }
-                else 
-                label2.Text = txt.Substring(0, counter);
+                else
+                    label2.Text = txt.Substring(0, counter);
             }
 
             private void label1_click(object sender, EventArgs e)
@@ -218,204 +145,17 @@ namespace CourseworkUOG_Quiz
 
         }
 
-        public partial class Bird : Form
-        {
-            public Label label1;
-            public Label label2;
-            public Timer timer1;
-            public Bird()
-            {
-                label1 = new Label();
-                label1.Font = new Font("ALGERIAN", 30, FontStyle.Bold);
-                label1.Size = new Size(150, 150);
-                label1.Location = new Point(115, 90);
-                label1.Text = "00";
-                this.Controls.Add(label1);
-                label1.Click += new EventHandler(label1_click);
-
-                label2 = new Label();
-                label2.Font = new Font("Segoe Script", 15, FontStyle.Bold);
-                label2.Size = new Size(300, 150);
-                label2.Location = new Point(10, 10);
-                label2.Text = "WELCOME TO QUIZ!..";
-                this.Controls.Add(label2);
-                label2.Click += new EventHandler(label2_click);
-
-                Timer timer1 = new Timer
-                {
-                    Interval = 500
-                };
-                timer1.Enabled = true;
-                timer1.Tick += new System.EventHandler(OnTimerEvent);
-
-                Load += new EventHandler(Bird_Load);
-
-            }
-            int counter = 0;
-            int len = 0;
-            string txt;
-            int timeleft = 30;
-            private void OnTimerEvent(object sender, EventArgs e)
-            {
-                if (timeleft > 0)
-                {
-                    timeleft = timeleft - 1;
-                    label1.Text = timeleft + "";
-                }
-
-                if (label1.Text == "0")
-                {
-                    var Form1 = new Form1();
-                    Hide();
-                    Form1.Show();
-                    Close();
-
-                }
-
-                counter++;
-                if (counter > len)
-                {
-                    counter = 0;
-                    label2.Text = "";
-                }
-                else
-                    label2.Text = txt.Substring(0, counter);
-            }
-
-            private void label1_click(object sender, EventArgs e)
-            {
-
-            }
-
-            private void label2_click(object sender, EventArgs e)
-            {
-
-            }
-
-            private void Bird_Load(object sender, EventArgs e)
-            {
-                txt = label2.Text;
-                len = txt.Length;
-                label2.Text = "";
-            }
-        }
-
-        public partial class AquaticAnimal : Form
-        {
-            public Label label1;
-            public Label label2;
-            public Timer timer1;
-            public AquaticAnimal()
-            {
-                label1 = new Label();
-                label1.Font = new Font("ALGERIAN", 30, FontStyle.Bold);
-                label1.Size = new Size(150, 150);
-                label1.Location = new Point(115, 90);
-                label1.Text = "00";
-                this.Controls.Add(label1);
-                label1.Click += new EventHandler(label1_click);
-
-                label2 = new Label();
-                label2.Font = new Font("Segoe Script", 15, FontStyle.Bold);
-                label2.Size = new Size(300, 150);
-                label2.Location = new Point(10, 10);
-                label2.Text = "WELCOME TO QUIZ!..";
-                this.Controls.Add(label2);
-                label2.Click += new EventHandler(label2_click);
-
-                Timer timer1 = new Timer
-                {
-                    Interval = 500
-                };
-                timer1.Enabled = true;
-                timer1.Tick += new System.EventHandler(OnTimerEvent);
-
-                Load += new EventHandler(Aquatic_Load);
-
-            }
-            int counter = 0;
-            int len = 0;
-            string txt;
-            int timeleft = 30;
-            private void OnTimerEvent(object sender, EventArgs e)
-            {
-                if (timeleft > 0)
-                {
-                    timeleft = timeleft - 1;
-                    label1.Text = timeleft + "";
-                }
-
-                if (label1.Text == "0")
-                {
-                    var Form1 = new Form1();
-                    Hide();
-                    Form1.Show();
-                    Close();
-
-                }
-
-                counter++;
-                if (counter > len)
-                {
-                    counter = 0;
-                    label2.Text = "";
-                }
-                else
-                    label2.Text = txt.Substring(0, counter);
-            }
-
-            private void label1_click(object sender, EventArgs e)
-            {
-
-            }
-
-            private void label2_click(object sender, EventArgs e)
-            {
-
-            }
-
-            private void Aquatic_Load(object sender, EventArgs e)
-            {
-                txt = label2.Text;
-                len = txt.Length;
-                label2.Text = "";
-            }
-        }
-      
-
-        public partial class Form1 : Form
+        public partial class Question : Form
         {
 
 
-            public Form1()
+            public Question()
+
             {
             }
-                
-
-        }
-
-        public partial class Form2 : Form
-        {
-
-
-            public Form2()
-            {
-            }
-
-
-        }
-
-        public partial class Form3 : Form
-        {
-
-
-            public Form3()
-            {
-            }
-
-
         }
     }
-}
+
+
     
 
