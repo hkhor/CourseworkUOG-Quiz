f2771a0a19e88a728ab48fb14638d46e1d73d401
sweemingtan.9 <sweemingtan.9@gmail.com>
2021-11-17 21:08:13 +0800

    Update Animation

--- CourseworkUOG-Quiz/Home.cs
+++ CourseworkUOG-Quiz/Home.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace CourseworkUOG_Quiz
 {
@@ -18,11 +19,14 @@ namespace CourseworkUOG_Quiz
         public RadioButton radioButton2;
         public RadioButton radioButton3;
         public Label label1;
+
+        
         public Home()
         {
             InitializeComponent();
 
         }
+
 
         private void Home_Load(object sender, EventArgs e)
         {
@@ -70,13 +74,18 @@ namespace CourseworkUOG_Quiz
 
         }
 
+
+        
         private void button1_click(object sender, EventArgs e)
         {
+
             if (radioButton1.Checked == true)
             {
                 var newform = new JungleAnimal();
                 newform.Show();
             }
+
+
             else if (radioButton2.Checked == true)
             {
                 var newform = new Bird();
@@ -88,7 +97,6 @@ namespace CourseworkUOG_Quiz
                 newform.Show();
             }
         }
-
         private void button2_click(object sender, EventArgs e)
         {
             DialogResult result = MessageBox.Show("Are you sure you wish to quit?", "Exit Application", MessageBoxButtons.YesNo);
@@ -127,29 +135,285 @@ namespace CourseworkUOG_Quiz
 
         public partial class JungleAnimal : Form
         {
+            public Label label1;
+            public Label label2;
+            public Timer timer1;
+
             public JungleAnimal()
 
             {
- 
 
+                label1 = new Label();
+                label1.Font= new Font("ALGERIAN", 30, FontStyle.Bold);
+                label1.Size = new Size(150, 150);
+                label1.Location = new Point(115, 90);
+                label1.Text = "00";
+                this.Controls.Add(label1);
+                label1.Click += new EventHandler(label1_click);
+
+                label2 = new Label();
+                label2.Font = new Font("Segoe Script", 15, FontStyle.Bold);
+                label2.Size = new Size(300, 150);
+                label2.Location = new Point(10, 10);
+                label2.Text = "WELCOME TO QUIZ!..";
+                this.Controls.Add(label2);
+                label2.Click += new EventHandler(label2_click);
+
+                Timer timer1 = new Timer
+                {
+                    Interval = 500
+                };
+                timer1.Enabled = true;
+                timer1.Tick += new System.EventHandler(OnTimerEvent);
+
+                Load += new EventHandler(JungleAnimal_Load);
+            }
+            int counter = 0;
+            int len = 0;
+            string txt;
+            int timeleft = 30;
+            private void OnTimerEvent(object sender,EventArgs e)
+            {
+                if (timeleft > 0)
+                {
+                    timeleft = timeleft - 1;
+                    label1.Text = timeleft + "";
+                }
+                
+                if (label1.Text == "0")
+                {
+                    var Form1 = new Form1();
+                    Hide();
+                    Form1.Show();
+                    Close();
+
+                }
+
+                counter++;
+                if(counter > len)
+                {
+                    counter = 0;
+                    label2.Text = "";
+                }
+                else 
+                label2.Text = txt.Substring(0, counter);
+            }
+
+            private void label1_click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void label2_click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void JungleAnimal_Load(object sender, EventArgs e)
+            {
+                txt = label2.Text;
+                len = txt.Length;
+                label2.Text = "";
             }
 
         }
 
         public partial class Bird : Form
         {
+            public Label label1;
+            public Label label2;
+            public Timer timer1;
             public Bird()
             {
+                label1 = new Label();
+                label1.Font = new Font("ALGERIAN", 30, FontStyle.Bold);
+                label1.Size = new Size(150, 150);
+                label1.Location = new Point(115, 90);
+                label1.Text = "00";
+                this.Controls.Add(label1);
+                label1.Click += new EventHandler(label1_click);
 
+                label2 = new Label();
+                label2.Font = new Font("Segoe Script", 15, FontStyle.Bold);
+                label2.Size = new Size(300, 150);
+                label2.Location = new Point(10, 10);
+                label2.Text = "WELCOME TO QUIZ!..";
+                this.Controls.Add(label2);
+                label2.Click += new EventHandler(label2_click);
+
+                Timer timer1 = new Timer
+                {
+                    Interval = 500
+                };
+                timer1.Enabled = true;
+                timer1.Tick += new System.EventHandler(OnTimerEvent);
+
+                Load += new EventHandler(Bird_Load);
+
+            }
+            int counter = 0;
+            int len = 0;
+            string txt;
+            int timeleft = 30;
+            private void OnTimerEvent(object sender, EventArgs e)
+            {
+                if (timeleft > 0)
+                {
+                    timeleft = timeleft - 1;
+                    label1.Text = timeleft + "";
+                }
+
+                if (label1.Text == "0")
+                {
+                    var Form1 = new Form1();
+                    Hide();
+                    Form1.Show();
+                    Close();
+
+                }
+
+                counter++;
+                if (counter > len)
+                {
+                    counter = 0;
+                    label2.Text = "";
+                }
+                else
+                    label2.Text = txt.Substring(0, counter);
+            }
+
+            private void label1_click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void label2_click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void Bird_Load(object sender, EventArgs e)
+            {
+                txt = label2.Text;
+                len = txt.Length;
+                label2.Text = "";
             }
         }
 
         public partial class AquaticAnimal : Form
         {
+            public Label label1;
+            public Label label2;
+            public Timer timer1;
             public AquaticAnimal()
+            {
+                label1 = new Label();
+                label1.Font = new Font("ALGERIAN", 30, FontStyle.Bold);
+                label1.Size = new Size(150, 150);
+                label1.Location = new Point(115, 90);
+                label1.Text = "00";
+                this.Controls.Add(label1);
+                label1.Click += new EventHandler(label1_click);
+
+                label2 = new Label();
+                label2.Font = new Font("Segoe Script", 15, FontStyle.Bold);
+                label2.Size = new Size(300, 150);
+                label2.Location = new Point(10, 10);
+                label2.Text = "WELCOME TO QUIZ!..";
+                this.Controls.Add(label2);
+                label2.Click += new EventHandler(label2_click);
+
+                Timer timer1 = new Timer
+                {
+                    Interval = 500
+                };
+                timer1.Enabled = true;
+                timer1.Tick += new System.EventHandler(OnTimerEvent);
+
+                Load += new EventHandler(Aquatic_Load);
+
+            }
+            int counter = 0;
+            int len = 0;
+            string txt;
+            int timeleft = 30;
+            private void OnTimerEvent(object sender, EventArgs e)
+            {
+                if (timeleft > 0)
+                {
+                    timeleft = timeleft - 1;
+                    label1.Text = timeleft + "";
+                }
+
+                if (label1.Text == "0")
+                {
+                    var Form1 = new Form1();
+                    Hide();
+                    Form1.Show();
+                    Close();
+
+                }
+
+                counter++;
+                if (counter > len)
+                {
+                    counter = 0;
+                    label2.Text = "";
+                }
+                else
+                    label2.Text = txt.Substring(0, counter);
+            }
+
+            private void label1_click(object sender, EventArgs e)
             {
 
             }
+
+            private void label2_click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void Aquatic_Load(object sender, EventArgs e)
+            {
+                txt = label2.Text;
+                len = txt.Length;
+                label2.Text = "";
+            }
+        }
+      
+
+        public partial class Form1 : Form
+        {
+
+
+            public Form1()
+            {
+            }
+                
+
+        }
+
+        public partial class Form2 : Form
+        {
+
+
+            public Form2()
+            {
+            }
+
+
+        }
+
+        public partial class Form3 : Form
+        {
+
+
+            public Form3()
+            {
+            }
+
+
         }
     }
 }
