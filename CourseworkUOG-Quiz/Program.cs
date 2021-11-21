3a952a711988fbe793ba2b1e4cbe026d89568d63
sweemingtan.9 <sweemingtan.9@gmail.com>
2021-11-21 19:51:33 +0800

    - update the interface

--- CourseworkUOG-Quiz/Program.cs
+++ CourseworkUOG-Quiz/Program.cs
@@ -16,7 +16,7 @@ namespace CourseworkUOG_Quiz
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Home());
         }
     }
 }
